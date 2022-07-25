95d0425f763208fbfb71056dad5777088b6a691f
Alejandra Ro <ale.ro.shegiwan@gmail.com>
2022-07-25 17:04:53 -0500

    checando lo responsive tambien

--- js/index.js
+++ js/index.js
@@ -1,76 +1,77 @@
 class Personaje {
-    constructor(name, image, homeworld, gender, cybernetics, affiliations) {
-        this.name = name;
-        this.image = image;
-        this.homeworld = homeworld;
-        this.gender = gender;
-        this.cybernetics = cybernetics;
-        this.affiliations = affiliations;
-    }
+  constructor(name, image, homeworld, gender, cybernetics, affiliations) {
+    this.name = name;
+    this.image = image;
+    this.homeworld = homeworld;
+    this.gender = gender;
+    this.cybernetics = cybernetics;
+    this.affiliations = affiliations;
+  }
 
-    obtenerNombre() {
-        return this.name;
-    }
-    obtenerFoto() {
-        return this.image;
-    }
-    obtenerOrigen() {
-       return this.homeworld;
-    }
-    obtenerGenero(){
-        return this.gender;
-    }
-    obtenerCiber(){
-      return this.cybernetics;
-    }
-    obtenerAlianzas(){
-      return this,affiliations;
-    }
+  obtenerNombre() {
+    return this.name;
+  }
+  obtenerFoto() {
+    return this.image;
+  }
+  obtenerOrigen() {
+    return this.homeworld;
+  }
+  obtenerGenero() {
+    return this.gender;
+  }
+  obtenerCiber() {
+    return this.cybernetics;
+  }
+  obtenerAlianzas() {
+    return this, affiliations;
+  }
 }
 
 let personajes = [];
 let elemento = document.getElementById("personajes-wrapper");
 
 async function getPersonajes() {
-    let url = "https://akabab.github.io/starwars-api/api/all.json";
+  let url = "https://akabab.github.io/starwars-api/api/all.json";
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    data.forEach(datum => {
-        let nuevoPersonaje = new Personaje(datum.name, datum.image, datum.homeworld, datum.gender, datum.affiliations)
-        personajes.push(nuevoPersonaje);
-    });
+  data.forEach(datum => {
+    let nuevoPersonaje = new Personaje(datum.name, datum.image, datum.homeworld, datum.gender, datum.affiliations)
+    personajes.push(nuevoPersonaje);
+  });
 
-    personajes.forEach((personaje) => {
+  personajes.forEach((personaje) => {
 
-        elemento.innerHTML += `
-        <div class="card " style="width: 18rem;">
-  <img src="${personaje.obtenerFoto()}" class="card-img-top h-50" alt="...">
-  <div class="card-body">
-    <h5 class="card-title" class="tpersonaje">${personaje.obtenerNombre()}</h5>
-    <p class="card-text">Origen ${personaje.obtenerOrigen()} Genero ${personaje.obtenerGenero()} </p>
-    <a href="#" class="btn btn-primary">Mas información</a>
-  </div>
-</div>
-        `
-    })
+    elemento.innerHTML += `
+            <div class="card " style="width: 18rem;">
+                <img src="${personaje.obtenerFoto()}" class="card-img-top h-50" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title" class="tpersonaje">${personaje.obtenerNombre()}</h5>
+                    <p class="card-text">Origen ${personaje.obtenerOrigen()} Genero ${personaje.obtenerGenero()}
+                    </p>
+                    <a href="#" class="btn btn-primary">Mas información</a>
+                </div>
+            </div>
+`
+  })
 }
 
 function reiniciarData() {
-    personajes.length = 0;
-    elemento.innerHTML = null;
-    getPersonajes();
+  personajes.length = 0;
+  elemento.innerHTML = null;
+  getPersonajes();
 }
 
 function llamarBusqueda() {
-    console.log('llamarBusqueda')
-    const consulta = document.getElementById("buscador").value;
-    const personajesFiltrados = personajes.filter(personaje => personaje.name === consulta)
+  console.log('llamarBusqueda')
+  const consulta = document.getElementById("buscador").value;
+  const personajesFiltrados = personajes.filter(personaje => personaje.name === consulta)
 
-    if (personajesFiltrados.length > 0) {
-        personajesFiltrados.forEach((personajeFiltrado) => {
-            elemento.innerHTML = `
+  if (personajesFiltrados.length > 0) {
+    personajesFiltrados.forEach((personajeFiltrado) => {
+      elemento.innerHTML = `
           <div class="card " style="width: 18rem;">
   <img src="${personajeFiltrado.obtenerFoto()}" class="card-img-top" alt="...">
   <div class="card-body h-50" >
@@ -80,11 +81,11 @@ function llamarBusqueda() {
   </div>
 </div>
         `
-        })
+    })
 
-    }
+  }
 
-   // console.log(consulta);
+  // console.log(consulta);
 
 }
 
@@ -171,5 +172,5 @@ class Personaje {
   getPersonajes();
 
 */
-  
-  
+
+
